70aced83014f08a331cce33683c664891fb8f2b7
vad9ss6 <vad9ss6@gmail.com>
2020-11-20 00:55:28 +0300

    refactoring reducer

--- src/Redux/profile-reducer.ts
+++ src/Redux/profile-reducer.ts
@@ -31,18 +31,17 @@ const initialState:ProfilePageType = {
 }
 
 export const profileReducer = (state: ProfilePageType = initialState, action: ActionProfileType) => {
+
     switch (action.type) {
         case 'ADD-POST': {
             const newPost = {id: 5, message: state.newPostText, likesCount: 0};
-            state.posts.push(newPost);
-            state.newPostText = '';
-
-            return state
+            state.newPostText = ''
+            return {...state, posts: [...state.posts, newPost]}
         }
         case 'UPDATE-NEW-POST-TEXT': {
-            state.newPostText = action.value
-
-            return state
+            const copyState = {...state}
+            copyState.newPostText = action.value
+            return copyState
         }
         default:
             return state
