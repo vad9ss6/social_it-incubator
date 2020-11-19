e84dfc9aa1c1711432fe7af9f381a5ce1751b7ef
vad9ss6 <vad9ss6@gmail.com>
2020-11-20 00:54:30 +0300

    refactoring Profile (context)

--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import  s from "./Profile.module.css";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import MyPostsContainer from "./MyPosts/MyPostsContainer";
+import {MyPostsContainer} from "./MyPosts/MyPostsContainer";
 
 
-type ProfilePropsType = {
-    store: any
-}
 
-const Profile = (props: ProfilePropsType) => {
+
+const Profile = () => {
     return(
         <div className={s.content}>
             <ProfileInfo />
-            <MyPostsContainer store={props.store}/>
+            <MyPostsContainer  />
         </div>
     )
 }
