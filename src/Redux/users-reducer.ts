a488ffcb4cf04017716ef5d0275b687afe3f1443
vad9ss6 <vad9ss6@gmail.com>
2020-11-22 22:30:12 +0300

    added usersReducer

---
+++ src/Redux/users-reducer.ts
@@ -0,0 +1,67 @@
+import { users } from "../Components/Users/Users";
+
+const FOLLOW = 'FOLLOW'
+const UNFOLLOW = 'UNFOLLOW'
+const SET_USERS = 'SET-USERS'
+
+type followActionType = {
+    type: 'FOLLOW'
+    id: number
+}
+type unFollowActionType = {
+    type: 'UNFOLLOW'
+    id: number
+}
+type setUsersActionType = {
+    type: 'SET-USERS'
+    users: Array<users>
+}
+
+
+type ActionDialogsType = followActionType | unFollowActionType | setUsersActionType
+
+type initialStateType = {
+    users: Array<users>
+}
+
+const initialState: initialStateType  = {
+    users: []
+}
+
+export const usersReducer = (state= initialState, action: ActionDialogsType) => {
+
+    switch (action.type) {
+        case FOLLOW:
+            return{
+                ...state,
+                users: state.users.map(u => {
+                    if(u.id === action.id){
+                       return {...u, followed: true}
+                    }
+                    return u
+                })
+            }
+        case UNFOLLOW:
+            return{
+                ...state,
+                users: state.users.map(u => {
+                    if(u.id === action.id){
+                        return {...u, followed: false}
+                    }
+                    return u
+                })
+            }
+        case SET_USERS:
+            return{
+
+                ...state,
+                users: [...state.users, ...action.users]
+            }
+        default:
+            return state
+    }
+}
+
+export const followAC = (id: number):followActionType  => ({type: FOLLOW, id})
+export const unFollowAC = (id: number):unFollowActionType  => ({type: UNFOLLOW, id})
+export const setUsersAC = (users: Array<users>):setUsersActionType  => ({type: SET_USERS, users})
