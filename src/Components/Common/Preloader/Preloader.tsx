7e78ca5effee51269c5643dac4a8bbbf8f438e4a
vad9ss6 <vad9ss6@gmail.com>
2020-12-14 22:37:23 +0300

    preloader

---
+++ src/Components/Common/Preloader/Preloader.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import preloader from "../../../assets/images/Spinner-1s-200px.svg";
+
+export const Preloader = () => {
+    return <img src={preloader} alt="preloader" style={{width: '100px'}}/>
+}
