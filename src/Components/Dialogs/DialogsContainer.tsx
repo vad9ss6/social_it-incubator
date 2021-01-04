f3fea5b190f931a5bf60fbc3d84fa4cd7adba345
vad9ss6 <vad9ss6@gmail.com>
2021-01-04 23:10:33 +0300

    ts mstp

--- src/Components/Dialogs/DialogsContainer.tsx
+++ src/Components/Dialogs/DialogsContainer.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import {addMessageAC, newMessageTextAC} from "../../Redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
 import {connect} from "react-redux";
+import {IGlobalState} from "../../Redux/redux-store";
 
-const mapStateToProps = (state: any) => {
+const mapStateToProps = (state: IGlobalState) => {
     return{
-        dialogsPage: state.dialogsPage
+        dialogsPage: state.dialogsPage,
+        isAuth: state.auth.isAuth
     }
 
 }
+
 const mapDispatchToProps = (dispatch: any) => {
     return {
         updateNewMessageText: (newMessage: string) => {
