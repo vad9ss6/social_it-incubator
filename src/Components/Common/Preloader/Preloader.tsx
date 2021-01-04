533ce7d95e3553f42ce1e704b3779a53d8261a0d
vad9ss6 <vad9ss6@gmail.com>
2021-01-04 20:14:49 +0300

    refactoring preloader

--- src/Components/Common/Preloader/Preloader.tsx
+++ src/Components/Common/Preloader/Preloader.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import preloader from "../../../assets/images/Spinner-1s-200px.svg";
+import s from "./preloader.module.css"
 
 export const Preloader = () => {
-    return <img src={preloader} alt="preloader" style={{width: '100px'}}/>
+    return <div className={s.main_block}>
+        <img className={s.main_img} src={preloader} alt="preloader" style={{width: '100px'}}/>
+    </div>
+
 }
