6a5c74f02ef49ce7cb7f5485ad66a342df1c4508
vad9ss6 <vad9ss6@gmail.com>
2021-01-04 20:22:17 +0300

    add style container

--- src/Components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import  s from "./ProfileInfo.module.css";
 import {userProfileType} from "../../../Redux/profile-reducer";
+import commonStyle from "../../Common/style/commonStyle.module.css";
 
 type ProfileInfoPropsType = {
     profile: userProfileType
@@ -8,7 +9,7 @@ type ProfileInfoPropsType = {
 
 const ProfileInfo = (props: ProfileInfoPropsType) => {
     return(
-        <div className={s.content}>
+        <div className={`${s.content} ${commonStyle.container}`}>
             <div>
                 {props.profile.photos.large ? <img src={props.profile.photos.large} alt=""/> : ''}
             </div>
