f3eebe9fac732906c53f2ac4ed95cd0241bd6112
vad9ss6 <vad9ss6@gmail.com>
2021-01-04 20:21:01 +0300

    added sunk authUser

--- src/Components/Header/HeaderContainer.tsx
+++ src/Components/Header/HeaderContainer.tsx
@@ -1,39 +1,30 @@
 import React from "react";
 import Header from "./Header";
 import {connect} from "react-redux";
-import {authType, setAuthUserDateAC} from "../../Redux/auth-reducer";
+import {authType, getAuthUser, setAuthUserDateAC} from "../../Redux/auth-reducer";
 import {IGlobalState} from "../../Redux/redux-store";
-import { usersAPI } from "../../api/api";
-
 
 type MDTPType = {
     setAuthUserDateAC: (data: authType) => void
+    getAuthUser: () => void
 }
 type MSTPType = {
     isAuth: boolean
     login: string | null
 }
-
 type PropsType = MDTPType & MSTPType
+
 class HeaderContainer extends React.Component<PropsType> {
     componentDidMount() {
-        usersAPI.AuthMe()
-            .then(response =>{
-                if(response.data.resultCode === 0){
-                    this.props.setAuthUserDateAC(response.data.data)
-                }
-            })
-
+        this.props.getAuthUser()
     }
-
     render(){
         return <Header {...this.props}/>
     }
-
 }
 const MSTPType = (state: IGlobalState):MSTPType => ({
     isAuth: state.auth.isAuth,
     login: state.auth.login
 });
-export default connect(MSTPType, {setAuthUserDateAC})(HeaderContainer)
+export default connect(MSTPType, {setAuthUserDateAC, getAuthUser})(HeaderContainer)
 
